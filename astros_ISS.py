8d015267e7eb26dba0449c10e9a193f5cc47bd77
qubit4 <82866970+qubit4@users.noreply.github.com>
2022-01-20 23:45:37 +0100

    Update astros_ISS.py

--- astros_ISS.py
+++ astros_ISS.py
@@ -4,14 +4,19 @@ def astros_info():
     import requests
 
     url = "http://api.open-notify.org/astros.json"
-
     response = requests.get(url)
-    jsonresponse = response.json()
-    inspace = jsonresponse["people"]
-    onISS = [item["name"]
-             for item in inspace if item["craft"] == "ISS"]
 
-    print("astronauts currently on the ISS are: \n", onISS)
+    if response.status_code == 200:
+
+        jsonresponse = response.json()
+        inspace = jsonresponse["people"]
+        onISS = [item["name"]
+                 for item in inspace if item["craft"] == "ISS"]
+        print("Astronauts currently on the ISS are: \n", onISS)
+
+    else:
+        print("Failed to read data. Status code is : {}".format(
+            response.status_code))
 
 
 astros_info()
