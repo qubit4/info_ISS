0cd0fe78ce8d35dd7ce3bde76a6611e665a98286
qubit4 <82866970+qubit4@users.noreply.github.com>
2022-02-11 17:24:57 +0100

    Update position_ISS.py

--- position_ISS.py
+++ position_ISS.py
@@ -1,21 +1,18 @@
-# provides current ISS coordinates
 import requests
 import pandas as pd
 
 
+# provides current ISS coordinates
 def position_info():
 
-    url = "http://api.open-notify.org/iss-now.json"
-    response = requests.get(url)
+    url1 = "http://api.open-notify.org/iss-now.json"
+    response = requests.get(url1)
 
     if response.status_code == 200:
-
         # dataframe
-        df = pd.read_json(url)
-        df["latitude"] = df.loc["latitude", "iss_position"]
-        df["longitude"] = df.loc["longitude", "iss_position"]
-        df.reset_index(inplace=True)
-        df = df.drop(["index", "message"], axis=1)
+        df = pd.read_json(url1)
+        df = df.drop(["message"], axis=1)
+        print("Current position of the International Space Station:")
         print(df)
 
     else:
@@ -23,4 +20,26 @@ def position_info():
             response.status_code))
 
 
+# prints the names of astronauts currently on the ISS
+def astros_info():
+
+    url2 = "http://api.open-notify.org/astros.json"
+    response = requests.get(url2)
+
+    if response.status_code == 200:
+
+        jsonresponse = response.json()
+        inspace = jsonresponse["people"]
+        onISS = [item["name"]
+                 for item in inspace if item["craft"] == "ISS"]
+        print("Astronauts currently on the ISS are:")
+        print(", ".join(onISS))
+
+    else:
+        print("Failed to read data. Status code is : {}".format(
+            response.status_code))
+
+
 position_info()
+print("\n")
+astros_info()
